6d974eed3ed2d15678847f023679c9d3e8e0555e
Momen_Aboessa <77535066+momenaboessa@users.noreply.github.com>
2021-11-01 00:32:32 +0200

    Update 202110260139595_SeedAdmin.cs

--- Migrations/202110260139595_SeedAdmin.cs
+++ Migrations/202110260139595_SeedAdmin.cs
@@ -8,7 +8,7 @@ namespace Artist.Migrations
         public override void Up()
         {
             Sql(@"
-                    INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'3a34b7a2-e1e3-4ec1-8882-da7a88a41ede', N'ebrahim@belal.com', 0, N'ACp6LgwAvHlUIitu3vCwEkCKthxXLTBs4uycybtXxt0d8xmBuusAFDMFQGx3MuRRHQ==', N'4e5dc47f-8a0b-4a9d-be04-f5a4c8c60f43', NULL, 0, 0, NULL, 1, 0, N'ebrahim@belal.com')
+                    INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'3a34b7a2-e1e3-4ec1-8882-da7a88a41ede', N'admin@admin.com', 0, N'ACp6LgwAvHlUIitu3vCwEkCKthxXLTBs4uycybtXxt0d8xmBuusAFDMFQGx3MuRRHQ==', N'4e5dc47f-8a0b-4a9d-be04-f5a4c8c60f43', NULL, 0, 0, NULL, 1, 0, N'ebrahim@belal.com')
                 ");
         }
         
